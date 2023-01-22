fe4bfb9383cd92ea33161c6e6172337ff3993922
Zdravko Šplajt <zdravek.sprite@gmail.com>
2023-01-22 13:31:29 +0100

    Laravel 2023 v0.6.8 [socialite]

--- resources/views/profile/partials/update-avatar-form.blade.php
+++ resources/views/profile/partials/update-avatar-form.blade.php
@@ -18,36 +18,67 @@
     <hr class="w-full">
   </div>
 
-  <div class="flex m-4 space-x-4">
-  <!-- Social Icons -->
-  @if ($user->google_id)
-    <img src="{{ $user->google_avatar }}" class="rounded-full w-24" alt="{{ $user->google_id }}" title="{{ $user->google_avatar }}">
-  @endif
-  @if ($user->facebook_id)
-    <img src="{{ $user->facebook_avatar }}" class="rounded-full w-24" alt="{{ $user->facebook_id }}" title="{{ $user->facebook_avatar }}">
-  @endif
-  @if ($user->twitter_id)
-    <img src="{{ $user->twitter_avatar }}" class="rounded-full w-24" alt="{{ $user->twitter_id }}" title="{{ $user->twitter_avatar }}">
-  @endif
-  @if ($user->github_id)
-    <img src="{{ $user->github_avatar }}" class="rounded-full w-24" alt="{{ $user->github_id }}" title="{{ $user->github_avatar }}">
-  @endif
-  </div>
+  <form method="POST" action="{{ route('admin.users.update', $user->id) }}">
+    @csrf
+    @method('PUT')
 
-  <div class="flex m-4 space-x-4">
-  <!-- Role Icons -->
-  @hasrole('superadmin')
-    <img src="https://upload.wikimedia.org/wikipedia/commons/5/55/User-admin-gear.svg" class="rounded-full w-24" alt="superadmin">
-  @endhasrole
-  @hasrole('admin')
-    <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/User_icon_1.svg" class="rounded-full w-24" alt="admin">
-  @endhasrole
-  @hasrole('socialuser')
-    <img src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Gnome-system-users.svg" class="rounded-full w-24" alt="socialuser">
-  @endhasrole
-  @hasrole('blockeduser')
-    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Gnome-stock_person.svg" class="rounded-full w-24" alt="blockeduser">
-  @endhasrole
-    <img src="https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg" class="rounded-full w-24" alt="user">
-  </div>
+    <div class="flex m-4 space-x-4">
+    <!-- Social Icons -->
+    @if ($user->google_id)
+    <x-label-radio :id="$user->google_id" name="avatars[]" :value="$user->google_avatar">
+      <img src="{{ $user->google_avatar }}" class="rounded-full w-24" alt="{{ $user->google_id }}" title="{{ $user->google_avatar }}">
+    </x-label-radio>
+    @endif
+    @if ($user->facebook_id)
+    <x-label-radio :id="$user->facebook_id" name="avatars[]" :value="$user->facebook_avatar">
+      <img src="{{ $user->facebook_avatar }}" class="rounded-full w-24" alt="{{ $user->facebook_id }}" title="{{ $user->facebook_avatar }}">
+    </x-label-radio>
+    @endif
+    @if ($user->twitter_id)
+    <x-label-radio :id="$user->twitter_id" name="avatars[]" :value="$user->twitter_avatar">
+      <img src="{{ $user->twitter_avatar }}" class="rounded-full w-24" alt="{{ $user->twitter_id }}" title="{{ $user->twitter_avatar }}">
+    </x-label-radio>
+    @endif
+    @if ($user->github_id)
+    <x-label-radio :id="$user->github_id" name="avatars[]" :value="$user->github_avatar">
+      <img src="{{ $user->github_avatar }}" class="rounded-full w-24" alt="{{ $user->github_id }}" title="{{ $user->github_avatar }}">
+    </x-label-radio>
+    @endif
+    </div>
+
+    <div class="flex m-4 space-x-4">
+    <!-- Role Icons -->
+    @hasrole('superadmin')
+    <x-label-radio id="superadmin-avatar" name="avatars[]" value="https://upload.wikimedia.org/wikipedia/commons/5/55/User-admin-gear.svg">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/5/55/User-admin-gear.svg" class="rounded-full w-24" alt="superadmin">
+    </x-label-radio>
+    @endhasrole
+    @hasrole('admin')
+    <x-label-radio id="admin-avatar" name="avatars[]" value="https://upload.wikimedia.org/wikipedia/commons/0/04/User_icon_1.svg">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/User_icon_1.svg" class="rounded-full w-24" alt="admin">
+    </x-label-radio>
+    @endhasrole
+    @hasrole('socialuser')
+    <x-label-radio id="socialuser-avatar" name="avatars[]" value="https://upload.wikimedia.org/wikipedia/commons/1/1d/Gnome-system-users.svg">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Gnome-system-users.svg" class="rounded-full w-24" alt="socialuser">
+    </x-label-radio>
+    @endhasrole
+    @hasrole('blockeduser')
+    <x-label-radio id="blockeduser-avatar" name="avatars[]" value="https://upload.wikimedia.org/wikipedia/commons/0/0a/Gnome-stock_person.svg">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Gnome-stock_person.svg" class="rounded-full w-24" alt="blockeduser">
+    </x-label-radio>
+    @endhasrole
+    <x-label-radio id="user-avatar" name="avatars[]" value="https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg" class="rounded-full w-24" alt="user">
+    </x-label-radio>
+    </div>
+
+    <div class="flex items-center gap-4">
+      <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+      @if (session('status') === 'avatar-updated')
+      <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Saved.') }}</p>
+      @endif
+    </div>
+  </form>
 </section>
