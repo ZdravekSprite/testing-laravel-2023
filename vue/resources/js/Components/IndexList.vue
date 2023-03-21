5b3ef6477060e913b159f9bd3c6021f0cf3405b4
Zdravko Šplajt <zdravek.sprite@gmail.com>
2023-03-21 22:49:48 +0100

    Laravel 2023 Vue v0.5.1

---
+++ vue/resources/js/Components/IndexList.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  labels: {
+    type: Array,
+    default: [],
+  },
+  elements: {
+    type: Array,
+    default: [],
+  },
+});
+</script>
+
+<template>
+  <table class="table-auto w-full">
+    <thead class="text-lg font-medium text-gray-900 dark:text-gray-100">
+      <tr>
+        <th v-if="elements[0].icon" class="w-8"></th>
+        <th v-for="l in labels" :key="l.id">{{ l }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="e in elements" :key="e.id">
+        <td v-if="e.icon"><img class="rounded-full shadow-xl" width="20" height="20" :src="e.icon" /></td>
+        <td v-for="l in labels" :key="l.id" class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+          {{ e[l] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
--- vue/resources/js/Pages/Role/Index.vue
+++ vue/resources/js/Pages/Role/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import IndexList from '@/Components/IndexList.vue';
 import { Head } from '@inertiajs/vue3';
 defineProps({
   roles: Array,
@@ -19,20 +20,7 @@ defineProps({
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-          <table class="table-auto w-full">
-            <thead class="text-lg font-medium text-gray-900 dark:text-gray-100">
-              <tr>
-                <th>Role</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-              v-for="r in roles"
-              :key="r.id">
-                <td class="mt-1 text-sm text-gray-600 dark:text-gray-400">{{r.name}}</td>
-              </tr>
-            </tbody>
-          </table>
+          <IndexList :elements="roles" :labels="['name','description']"/>
         </div>
       </div>
     </div>
